a144d9d5542309fd0f8d3fdee946370dd201408b
stijnpinson <stijnpinson@gmail.com>
2014-01-20 19:43:53 +0100

    GuardCameraTest aangemaakt

---
+++ src/test/GuardCameraTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import MazeRunner.Opponents.GuardCamera;
+
+public class GuardCameraTest {
+
+	GuardCamera testcamera = new GuardCamera(3, 2, 3, runner);
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
